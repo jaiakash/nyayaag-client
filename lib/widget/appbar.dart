b0ff2f615fde44ace89534111279352cc4812d5a
Akash Jaiswal <akashjaiswal3846@gmail.com>
2022-05-12 01:52:14 +0530

    fix: app bar

--- lib/widget/appbar.dart
+++ lib/widget/appbar.dart
@@ -11,66 +11,87 @@ class NyayaagAppBarWidget {
       ),
       actions: <Widget>[
         const SizedBox(width: 10),
-        OutlinedButton(
-          onPressed: null,
-          style: ButtonStyle(
-            shape: MaterialStateProperty.all(RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0))),
-          ),
+        ElevatedButton(
+          onPressed: () {
+            debugPrint('Received click');
+          },
+          style: ElevatedButton.styleFrom(
+              primary: Colors.green.shade900,
+              fixedSize: const Size(150, 75),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10))),
           child: const Text("Home"),
         ),
-        const SizedBox(width: 10),
-        OutlinedButton(
-          onPressed: null,
-          style: ButtonStyle(
-            shape: MaterialStateProperty.all(RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0))),
-          ),
+        const SizedBox(width: 20),
+        ElevatedButton(
+          onPressed: () {
+            debugPrint('Received click');
+          },
+          style: ElevatedButton.styleFrom(
+              primary: Colors.green.shade900,
+              fixedSize: const Size(150, 75),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10))),
           child: const Text("Our Services"),
         ),
         const SizedBox(width: 10),
-        OutlinedButton(
-          onPressed: null,
-          style: ButtonStyle(
-            shape: MaterialStateProperty.all(RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0))),
-          ),
+        ElevatedButton(
+          onPressed: () {
+            debugPrint('Received click');
+          },
+          style: ElevatedButton.styleFrom(
+              primary: Colors.green.shade900,
+              fixedSize: const Size(150, 75),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10))),
           child: const Text("Career"),
         ),
         const SizedBox(width: 10),
-        OutlinedButton(
-          onPressed: null,
-          style: ButtonStyle(
-            shape: MaterialStateProperty.all(RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0))),
-          ),
+        ElevatedButton(
+          onPressed: () {
+            debugPrint('Received click');
+          },
+          style: ElevatedButton.styleFrom(
+              primary: Colors.green.shade900,
+              fixedSize: const Size(150, 75),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10))),
           child: const Text("Blog"),
         ),
         const SizedBox(width: 10),
-        OutlinedButton(
-          onPressed: null,
-          style: ButtonStyle(
-            shape: MaterialStateProperty.all(RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0))),
-          ),
+        ElevatedButton(
+          onPressed: () {
+            debugPrint('Received click');
+          },
+          style: ElevatedButton.styleFrom(
+              primary: Colors.green.shade900,
+              fixedSize: const Size(150, 75),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10))),
           child: const Text("Gallery"),
         ),
         const SizedBox(width: 10),
-        OutlinedButton(
-          onPressed: null,
-          style: ButtonStyle(
-            shape: MaterialStateProperty.all(RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0))),
-          ),
+        ElevatedButton(
+          onPressed: () {
+            debugPrint('Received click');
+          },
+          style: ElevatedButton.styleFrom(
+              primary: Colors.green.shade900,
+              fixedSize: const Size(150, 75),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10))),
           child: const Text("Consult Expert"),
         ),
         const SizedBox(width: 10),
-        OutlinedButton(
-          onPressed: null,
-          style: ButtonStyle(
-            shape: MaterialStateProperty.all(RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0))),
-          ),
+        ElevatedButton(
+          onPressed: () {
+            debugPrint('Received click');
+          },
+          style: ElevatedButton.styleFrom(
+              primary: Colors.green.shade900,
+              fixedSize: const Size(150, 75),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10))),
           child: const Text("Login/Register"),
         ),
         const SizedBox(width: 50),
