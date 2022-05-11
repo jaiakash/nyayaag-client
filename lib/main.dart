60f424e294e11ae7c3682349eaa635e142131440
Akash Jaiswal <akashjaiswal3846@gmail.com>
2022-05-12 04:14:44 +0530

    chore: login page

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class NyayaagApp extends StatelessWidget {
         '/home': (context) =>
             const HomePage(title: 'Nyayaag - Voice of Justice'),
         '/citizen/login': (context) =>
-            const LoginPage(title: 'Nyayaa - Citizen Login'),
+            const LoginPage(title: 'Nyayaag - Citizen Login'),
         '/advocate/login': (context) =>
             const LoginPage(title: 'Nyayaag - Advocate Login'),
       },
